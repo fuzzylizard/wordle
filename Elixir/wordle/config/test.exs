dff201ab83adf6fe9a168faa440266678e25c5a9
Christopher Johnston <chris@fuzzylizard.com>
2024-02-05 16:13:15 -0500

    fixes config for test database

--- Elixir/wordle/config/test.exs
+++ Elixir/wordle/config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :wordle, Wordle.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "chris",
+  password: "",
   hostname: "localhost",
   database: "wordle_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
